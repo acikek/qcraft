053e8f87c8c962516828fc654d7a99c57c37c49d
acikek <spiralixp@gmail.com>
2022-02-23 19:29:48 -0800

    goggles more

--- src/main/java/com/acikek/qcraft/QCraft.java
+++ src/main/java/com/acikek/qcraft/QCraft.java
@@ -2,10 +2,10 @@ package com.acikek.qcraft;
 
 import com.acikek.qcraft.block.Blocks;
 import com.acikek.qcraft.block.QuantumOre;
-import com.acikek.qcraft.recipe.EntangledPairRecipe;
-import com.acikek.qcraft.recipe.QBlockRecipe;
 import com.acikek.qcraft.command.ClearQBlocksCommand;
 import com.acikek.qcraft.item.Items;
+import com.acikek.qcraft.recipe.EntangledPairRecipe;
+import com.acikek.qcraft.recipe.QBlockRecipe;
 import com.acikek.qcraft.world.QBlockTickListener;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
--- src/main/java/com/acikek/qcraft/item/Goggles.java
+++ src/main/java/com/acikek/qcraft/item/Goggles.java
@@ -3,19 +3,77 @@ package com.acikek.qcraft.item;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 
 public class Goggles extends ArmorItem {
 
     public enum Type {
-        QUANTUM,
-        ANTI_OBSERVATION
+        QUANTUM("quantum"),
+        ANTI_OBSERVATION("anti_observation");
+
+        public String id;
+
+        Type(String id) {
+            this.id = id;
+        }
+    }
+
+    public static class Material implements ArmorMaterial {
+
+        public Type type;
+
+        public Material(Type type) {
+            this.type = type;
+        }
+
+        @Override
+        public int getDurability(EquipmentSlot slot) {
+            return -1;
+        }
+
+        @Override
+        public int getProtectionAmount(EquipmentSlot slot) {
+            return 0;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 0;
+        }
+
+        @Override
+        public SoundEvent getEquipSound() {
+            return SoundEvents.ITEM_ARMOR_EQUIP_CHAIN;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return type.id + "_goggles";
+        }
+
+        @Override
+        public float getToughness() {
+            return 0;
+        }
+
+        @Override
+        public float getKnockbackResistance() {
+            return 0;
+        }
     }
 
     public Type type;
 
     public Goggles(Type type) {
-        super(ArmorMaterials.LEATHER, EquipmentSlot.HEAD, Items.defaultSettings());
+        super(new Material(type), EquipmentSlot.HEAD, Items.defaultSettings());
         this.type = type;
     }
 
