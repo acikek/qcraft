f8efaece472806c48ea0b2fc8cc7295f2bc5e66e
acikek <spiralixp@gmail.com>
2022-02-24 12:14:19 -0800

    display of automated observer

--- src/main/java/com/acikek/qcraft/block/AutomatedObserver.java
+++ src/main/java/com/acikek/qcraft/block/AutomatedObserver.java
@@ -10,6 +10,9 @@ import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class AutomatedObserver extends AbstractRedstoneGateBlock {
 
@@ -19,9 +22,26 @@ public class AutomatedObserver extends AbstractRedstoneGateBlock {
 
     public AutomatedObserver() {
         super(DEFAULT_SETTINGS);
-        getStateManager().getDefaultState()
-                .with(FACING, Direction.SOUTH)
-                .with(POWERED, false);
+        setDefaultState(
+                getStateManager().getDefaultState()
+                    .with(FACING, Direction.SOUTH)
+                    .with(POWERED, false)
+        );
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (!state.get(POWERED)) {
+            return;
+        }
+        Direction direction = state.get(FACING);
+        double d = (double) pos.getX() + 0.5 + (random.nextDouble() - 0.5) * 0.2;
+        double e = (double) pos.getY() + 0.4 + (random.nextDouble() - 0.5) * 0.2;
+        double f = (double) pos.getZ() + 0.5 + (random.nextDouble() - 0.5) * 0.2;
+        float g = -5.0f;
+        double h = (g /= 16.0f) * (float) direction.getOffsetX();
+        double i = g * (float) direction.getOffsetZ();
+        world.addParticle(QuantumOre.DUST_PARTICLE, d + h, e, f + i, 0.0, 0.0, 0.0);
     }
 
     @Override
--- src/main/java/com/acikek/qcraft/client/QCraftClient.java
+++ src/main/java/com/acikek/qcraft/client/QCraftClient.java
@@ -1,13 +1,17 @@
 package com.acikek.qcraft.client;
 
+import com.acikek.qcraft.block.Blocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class QCraftClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.AUTOMATED_OBSERVER, RenderLayer.getCutout());
     }
 }
